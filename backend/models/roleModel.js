52fd6ec63d30cd91ce55490d16419c53bb72e17d
Ethan Scherwitz <ehallscherwitz@gmail.com>
2025-03-23 20:19:31 -0500

    added a couple more routes

--- backend/models/roleModel.js
+++ backend/models/roleModel.js
@@ -5,6 +5,11 @@ const getAllRoles = async () => {
     return result.rows;
 };
 
+const getRoleById = async (id) => {
+    const result = await pool.query('SELECT * FROM roles WHERE id = $1', [id]);
+    return result.rows[0];
+};
+
 module.exports = {
-    getAllRoles
+    getAllRoles, getRoleById
 };
